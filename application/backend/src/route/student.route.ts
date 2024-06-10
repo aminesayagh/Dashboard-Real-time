2883544188003c2b5434e213707138b2d00856dd
Mouad Imam <74561965+Mouados-byte@users.noreply.github.com>
2024-06-10 22:45:12 +0000

    Fixing Student Routing bugs

--- application/backend/src/route/student.route.ts
+++ application/backend/src/route/student.route.ts
@@ -1,27 +1,31 @@
 import express from 'express';
 import { ERRORS } from '../constants/ERRORS';
-import UniversityPeriodModel, { IUniversityPeriodDocument } from '../model/UniversityPeriod.model';
+import StudentModel, { IStudentDocument } from '../model/Student.model';
 import { ApiRequest, ApiResponse } from 'types/Api';
 const router = express.Router();
 
-router.post('/students', async (req: ApiRequest, res: ApiResponse<IUniversityPeriodDocument>): Promise<void> => {
-    const universityPeriod = new UniversityPeriodModel(req.body);
-    const result = await universityPeriod.save().catch((err) => {
+router.post('/', async (req: ApiRequest, res: ApiResponse<IStudentDocument>): Promise<void> => {
+    try {
+        const student = new StudentModel(req.body);
+        const result = await student.save();
+
+        if (!result) {
+            res.status(500).send({ status: 'error', message: ERRORS.INTERNAL_SERVER_ERROR });
+            return;
+        }
+
+        res.send({
+            status: 'success',
+            data: result
+        });
+    } catch (err:any) {
         res.status(400).send({ status: 'error', message: err.message });
-    });
-    if (!result) {
-        res.status(500).send({ status: 'error', message: ERRORS.INTERNAL_SERVER_ERROR });
-        return;
     }
-    res.send({
-        status: 'success',
-        data: result
-    });
 });
 
-router.put('/students/:id', async (req: ApiRequest<Partial<IUniversityPeriodDocument>, {}, { id: string }>, res: ApiResponse<IUniversityPeriodDocument>) => {
+router.put('/:id', async (req: ApiRequest<Partial<IStudentDocument>, {}, { id: string }>, res: ApiResponse<IStudentDocument>) => {
     const { id } = req.params;
-    const result = await UniversityPeriodModel.findByIdAndUpdate(id, req.body).catch((err) => {
+    const result = await StudentModel.findByIdAndUpdate(id, req.body).catch((err) => {
         res.status(400).send({ status: 'error', message: err.message });
     });
     if (!result) {
