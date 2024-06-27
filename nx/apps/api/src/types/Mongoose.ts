9724f78c736a4c379d55d3aba79670a54653db6e
aminesayagh <aminesayagh1997@gmail.com>
2024-06-27 08:58:36 +0000

    Improve shared typesafety of the application in front end

--- nx/apps/api/src/types/Mongoose.ts
+++ nx/apps/api/src/types/Mongoose.ts
@@ -1,29 +1,8 @@
 import { Types, Document } from 'mongoose';
-import { BaseDocument as ExternalBaseDocument, DefaultDocument as ExternalDefaultDocument } from 'shared-ts'
-
-export type BaseDocument<T> = ExternalBaseDocument<T, Types.ObjectId>; 
-export type DefaultDocument<T> = ExternalDefaultDocument<T, Types.ObjectId, Document<T>>;
 
 
-export type PublicDocument<T> = ReturnType<DefaultDocument<T>["toObject"]>;
 
-// Type for the object form of the document
-export type ObjectDocument<T> = ReturnType<DefaultDocument<T>["toObject"]>;
-
-// Function to convert a Mongoose document to an object with specific fields
-export const toObject = <T>(doc: DefaultDocument<T>): ObjectDocument<T> => {
-  const obj = doc.toObject();
-  obj._id = doc._id;
-  obj.created_at = doc.created_at;
-  obj.updated_at = doc.updated_at;
-  return obj as ObjectDocument<T>;
-};
-
-// Function to convert an array of Mongoose documents to objects
-export const toObjectArray = <T>(docs: DefaultDocument<T>[]): ObjectDocument<T>[] => 
-  docs.map(toObject);
-
-export interface PublicDoc<T extends Document> {
+export interface PublicDoc<T extends Document<Types.ObjectId>> {
   id: string;
   doc: Omit<T, '_id' | 'createdAt' | 'updatedAt'>;
   createdAt: Date;
@@ -40,4 +19,10 @@ export function toPublicDoc<T extends Document<Types.ObjectId, any, T>>(doc: T):
     createdAt: createdAt,
     updatedAt: createdAt,
   };
+}
+
+export type PublicDocArray<T extends Document<Types.ObjectId>> = PublicDoc<T>[];
+
+export function toPublicDocArray<T extends Document<Types.ObjectId, any, T>>(docs: T[]): PublicDocArray<T> {
+  return docs.map(toPublicDoc);
 }
